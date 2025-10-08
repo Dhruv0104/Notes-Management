0d07f332964abce910fd6ea299d867ff1e30750b
Dhruv0104 <dvpatel6048@gmail.com>
2025-10-08 19:59:45 +0530

    [FIX] deploy vite config

--- client-web/vite.config.js
+++ client-web/vite.config.js
@@ -4,5 +4,17 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	build: {
+		rollupOptions: {
+			output: {
+				manualChunks(id) {
+					if (id.includes('node_modules')) {
+						return 'vendor';
+					}
+				},
+			},
+		},
+		chunkSizeWarningLimit: 3000, // increase limit to silence warnings or fix code splitting
+	},
 	root: './',
 });
